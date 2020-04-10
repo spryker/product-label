f2e6025c679d94744730d384b27b5a6adc172671
AntonKhabiuk <brotherosirus@gmail.com>
2020-04-10 15:05:36 +0300

    CC-8839 Refactoring due to new P&S structure

--- src/Spryker/Zed/ProductLabel/Persistence/ProductLabelRepositoryInterface.php
+++ src/Spryker/Zed/ProductLabel/Persistence/ProductLabelRepositoryInterface.php
@@ -7,6 +7,7 @@
 
 namespace Spryker\Zed\ProductLabel\Persistence;
 
+use Generated\Shared\Transfer\FilterTransfer;
 use Generated\Shared\Transfer\ProductLabelTransfer;
 
 interface ProductLabelRepositoryInterface
@@ -17,4 +18,23 @@ interface ProductLabelRepositoryInterface
      * @return \Generated\Shared\Transfer\ProductLabelTransfer|null
      */
     public function findProductLabelById(int $idProductLabel): ?ProductLabelTransfer;
+
+    /**
+     * @param int[] $productAbstractIds
+     *
+     * @return \Generated\Shared\Transfer\ProductLabelProductAbstractTransfer[]
+     */
+    public function getProductLabelProductAbstractsByProductAbstractIds(array $productAbstractIds): array;
+
+    /**
+     * @param \Generated\Shared\Transfer\FilterTransfer $filterTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductLabelProductAbstractTransfer[]
+     */
+    public function getProductLabelProductAbstractsByFilter(FilterTransfer $filterTransfer): array;
+
+    /**
+     * @return \Generated\Shared\Transfer\ProductLabelLocalizedAttributesTransfer[]
+     */
+    public function getProductLabelLocalizedAttributes(): array;
 }
